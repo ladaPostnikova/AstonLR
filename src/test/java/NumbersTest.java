1cd5d84c12e5b8ae6b6d231713424138350344e3
ladaPostnikova <lada.postnikova03@mail.ru>
2025-03-24 20:28:57 +0300

    2

--- src/test/java/NumbersTest.java
+++ src/test/java/NumbersTest.java
@@ -1,45 +1,60 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class NumbersTest {
+
     @Test
-    void factorialNumbersTest() {
+    public void testFactorialOfZero() {
         assertEquals(FactorialNumbers.factorialNumbers(0), 1);
-        assertEquals(FactorialNumbers.factorialNumbers(1), 1);
+    }
+
+    @Test
+    public void testFactorialOfFive() {
         assertEquals(FactorialNumbers.factorialNumbers(5), 120);
     }
 
-    @Test
-    void areaOfTriangleTest(){
-        assertEquals(AreaOfTriangle.areaOfTriange(6,4),12);
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testNegativeInput() {
+        FactorialNumbers.factorialNumbers(-1);
     }
 
-    @Test
-    void calculatorAddTest(){
-        assertEquals(CalculatorNumber.add(5,6), 11);
-    }
-
-    @Test
-    void calculatorSubtractTest(){
-        assertEquals(CalculatorNumber.subtract(10,5), 5);
-    }
-
-    @Test
-    void calculatorDivisionTest(){
-        assertEquals(CalculatorNumber.division(12,6), 2);
-    }
-
-    @Test
-    void calculatorMultiplicationTest(){
-        assertEquals(CalculatorNumber.multiplication(6,4), 24);
-    }
-
-
-    @Test
-    void comparisonOfNumbersTest(){
-        assertEquals("Числа равны", ComparisonOfNumbers.comparisonOfNumbers(5,5));
-        assertEquals("Первое число больше", ComparisonOfNumbers.comparisonOfNumbers(6,5));
-        assertEquals("Второе число больше", ComparisonOfNumbers.comparisonOfNumbers(5,6));
-    }
+//    @Test
+//    void factorialNumbersTest() {
+//        assertEquals(FactorialNumbers.factorialNumbers(0), 1);
+//        assertEquals(FactorialNumbers.factorialNumbers(1), 1);
+//        assertEquals(FactorialNumbers.factorialNumbers(5), 120);
+//    }
+//
+//    @Test
+//    void areaOfTriangleTest(){
+//        assertEquals(AreaOfTriangle.areaOfTriange(6,4),12);
+//    }
+//
+//    @Test
+//    void calculatorAddTest(){
+//        assertEquals(CalculatorNumber.add(5,6), 11);
+//    }
+//
+//    @Test
+//    void calculatorSubtractTest(){
+//        assertEquals(CalculatorNumber.subtract(10,5), 5);
+//    }
+//
+//    @Test
+//    void calculatorDivisionTest(){
+//        assertEquals(CalculatorNumber.division(12,6), 2);
+//    }
+//
+//    @Test
+//    void calculatorMultiplicationTest(){
+//        assertEquals(CalculatorNumber.multiplication(6,4), 24);
+//    }
+//
+//
+//    @Test
+//    void comparisonOfNumbersTest(){
+//        assertEquals("Числа равны", ComparisonOfNumbers.comparisonOfNumbers(5,5));
+//        assertEquals("Первое число больше", ComparisonOfNumbers.comparisonOfNumbers(6,5));
+//        assertEquals("Второе число больше", ComparisonOfNumbers.comparisonOfNumbers(5,6));
+//    }
 
 }
