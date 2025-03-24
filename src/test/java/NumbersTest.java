718e0324bb189d57dd6c01708443e679696e2691
ladaPostnikova <lada.postnikova03@mail.ru>
2025-03-24 19:53:04 +0300

    Lesson_12_junit_5!

--- src/test/java/NumbersTest.java
+++ src/test/java/NumbersTest.java
@@ -1,5 +1,5 @@
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumbersTest {
     @Test
