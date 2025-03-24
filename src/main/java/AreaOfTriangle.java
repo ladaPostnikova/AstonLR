76053429f9e6f4f3da5da195c647cfef2b8dae76
ladaPostnikova <lada.postnikova03@mail.ru>
2025-03-24 18:53:24 +0300

    Lesson_12_junit_5

---
+++ src/main/java/AreaOfTriangle.java
@@ -0,0 +1,10 @@
+public class AreaOfTriangle {
+    public static double areaOfTriange (int leg1, int leg2) {
+        if (leg1 <= 0 || leg2 <= 0) {
+            System.out.println("Длина катета не может быть отрицательным числом или равным 0!");
+        }
+        double result = (leg1 * leg2) / 2;
+        System.out.println("Площадь тругольника: " + result);
+        return result;
+    }
+}
